21e9e376b9dfe304e7c8038a2f9f9bdff1a2908d
Eir03 <staroverovdanila@gmail.com>
2022-03-25 09:09:09 +0300

    Сделал окна авторизации и авторизации

---
+++ Сourse/Pages/PageAuzhorization.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Сourse.Classes;
+
+namespace Сourse.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageAuzhorization.xaml
+    /// </summary>
+    public partial class PageAuzhorization : Page
+    {
+        public PageAuzhorization()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSign_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TbReg_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            PageClass.frmAuthorization.Navigate(new PageRegistration());
+        }
+    }
+}
---
+++ Сourse/Pages/PageRegistration.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Сourse.Classes;
+
+namespace Сourse.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageRegistration.xaml
+    /// </summary>
+    public partial class PageRegistration : Page
+    {
+        public PageRegistration()
+        {
+            InitializeComponent();
+        }
+
+        private void TbBack_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            PageClass.frmAuthorization.GoBack();
+        }
+
+        private void BtnReg_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
